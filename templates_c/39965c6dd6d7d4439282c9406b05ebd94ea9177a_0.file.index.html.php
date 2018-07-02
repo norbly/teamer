f104964313669547031ab6b4c4d3671ff994727a
norbly <noutria@protonmail.com>
2018-07-02 11:33:46 +0200

    page add new event: added js to display/hide checkboxes depending on checked checkboxes

--- templates_c/39965c6dd6d7d4439282c9406b05ebd94ea9177a_0.file.index.html.php
+++ templates_c/39965c6dd6d7d4439282c9406b05ebd94ea9177a_0.file.index.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-07-02 10:43:08
+/* Smarty version 3.1.32, created on 2018-07-02 11:10:37
   from '/var/www/html/teamer/templates/index.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b39e59cb68e17_52026571',
+  'unifunc' => 'content_5b39ec0d2e5281_62915605',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '39965c6dd6d7d4439282c9406b05ebd94ea9177a' => 
     array (
       0 => '/var/www/html/teamer/templates/index.html',
-      1 => 1530520986,
+      1 => 1530520993,
       2 => 'file',
     ),
   ),
@@ -20,9 +20,8 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b39e59cb68e17_52026571 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b39ec0d2e5281_62915605 (Smarty_Internal_Template $_smarty_tpl) {
 echo $_GET['message'];?>
 
-<p>index</p>
-hi<?php }
+<p>index</p><?php }
 }
--- templates_c/f4857986bed26207af1c70ff4eadddf70d106986_0.file.add_event.html.php
+++ templates_c/f4857986bed26207af1c70ff4eadddf70d106986_0.file.add_event.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-07-02 10:46:05
+/* Smarty version 3.1.32, created on 2018-07-02 11:32:17
   from '/var/www/html/teamer/templates/add_event.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b39e64d13c332_56443640',
+  'unifunc' => 'content_5b39f121a7cc19_97574875',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'f4857986bed26207af1c70ff4eadddf70d106986' => 
     array (
       0 => '/var/www/html/teamer/templates/add_event.html',
-      1 => 1530521161,
+      1 => 1530523935,
       2 => 'file',
     ),
   ),
@@ -20,24 +20,91 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b39e64d13c332_56443640 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b39f121a7cc19_97574875 (Smarty_Internal_Template $_smarty_tpl) {
 ?><h1>add a new event</h1>
 <form action="<?php echo $_smarty_tpl->tpl_vars['SCRIPT_NAME']->value;?>
 ?action=add_event" method="post">
-   <label for="title">title</label><input type="text" name="title" id="title"><?php echo $_smarty_tpl->tpl_vars['ERROR_TITLE']->value;?>
+    <div>
+        <label for="title">title</label><input type="text" name="title" id="title"><?php echo $_smarty_tpl->tpl_vars['ERROR_TITLE']->value;?>
 </br>
-    description: </br><textarea name="description" id="description" cols="30" rows="10"></textarea><?php echo $_smarty_tpl->tpl_vars['ERROR_DESCRIPTION']->value;?>
+    </div>
+   <div>
+       description: </br><textarea name="description" id="description" cols="30" rows="10"></textarea><?php echo $_smarty_tpl->tpl_vars['ERROR_DESCRIPTION']->value;?>
 </br>
-    <input type="checkbox" name="fixed_date" id=""checked> <label for="fixed_date">fixes Datum</label></br>
-    <input type="date" name="starte_date" id="">
-    <input type="time" name="start_time" id=""></br>
-    <input type="checkbox" name="fixed_location" id="" checked>fixer Ort </br>
-    <input type="checkbox" name="limited_number_of_participants" id="" value="1">begrenzte Teilnehmeranzahl</br>
-    number_of_participants:<input type="number" name="max_number_of_participants" id=""></br>
-    <input type="checkbox" name="advance_reservation_required" id="">advance_reservation_required</br>
-    <input type="checkbox" name="confirm_reservations" id="" >confirm_reservations</br>
+   </div> 
+    <div>
+        <input type="checkbox" name="fixed_date" id="fixed_date"checked> <label for="fixed_date">fixes Datum</label>
+    </div>
+    <div>
+        <input type="date" name="start_date" id="start_date">
+        <input type="time" name="start_time" id="">
+    </div>
+    <div>
+        <input type="checkbox" name="fixed_location" id="fixed_location" checked>fixer Ort
+        
+    </div>  
+    <div>
+        Ort: <input type="text" name="location" id="location">
+    </div>
+    <div>
+        <input type="checkbox" name="limited_number_of_participants" id="limited_number_of_participants" checked>begrenzte Teilnehmeranzahl
+    </div>
+    <div>
+        number_of_participants:<input type="number" name="max_number_of_participants" id="max_number_of_participants">
+    </div>
+    <div>
+        <input type="checkbox" name="advance_reservation_required" id="advance_reservation_required" checked>advance_reservation_required
+    </div>
+    <div>
+        <input type="checkbox" name="confirm_reservations" id="confirm_reservations" >confirm_reservations
+    </div>
     <input type="submit" value="create event"> 
     
 </form>
+
+<?php echo '<script'; ?>
+>
+    var input_fixed_date = document.getElementById('fixed_date');
+    var input_start_date = document.getElementById('start_date');
+    var input_fixed_location = document.getElementById('fixed_location');
+    var input_location = document.getElementById('location');
+    var input_limited_number_of_participants = document.getElementById('limited_number_of_participants');
+    var input_max_number_of_participants = document.getElementById('max_number_of_participants');
+    var input_advance_reservation_required = document.getElementById('advance_reservation_required');
+    var input_confirm_reservations = document.getElementById('confirm_reservations');
+
+    input_fixed_date.addEventListener('click', function() {
+        if (input_fixed_date.checked) {
+            input_start_date.parentNode.style.display = "block";
+        } else {
+            input_start_date.parentNode.style.display = "none";
+        }
+    });
+
+    input_fixed_location.addEventListener('click', function() {
+        if (input_fixed_location.checked) {
+            input_location.parentNode.style.display = "block";
+        } else {
+            input_location.parentNode.style.display = "none";
+        }
+    });
+
+    input_limited_number_of_participants.addEventListener('click', function() {
+        if (input_limited_number_of_participants.checked) {
+            input_max_number_of_participants.parentNode.style.display = "block";
+        } else {
+            input_max_number_of_participants.parentNode.style.display = "none";
+        }
+    });
+
+    input_advance_reservation_required.addEventListener("click", function() {
+        if (input_advance_reservation_required.checked) {
+            input_confirm_reservations.parentNode.style.display = "block";
+        } else {
+            input_confirm_reservations.parentNode.style.display = "none";
+        }
+    })
+<?php echo '</script'; ?>
+>
 <?php }
 }
